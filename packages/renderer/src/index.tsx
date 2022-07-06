eda18e3e6da6779af24a3b0eeb837c3babc47e86
João Marco <moaojarco@gmail.com>
2022-07-06 05:12:17 -0400

    Remove old render react

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -2,8 +2,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import App from './app'
+import {createRoot} from 'react-dom/client'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') 
 )
+
+const container = document.getElementById('root')
+
+const root = createRoot(container)
+
+root.render(<App tab="home" />)
+
